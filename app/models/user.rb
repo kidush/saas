6fd34b5b677281796b35a7da1ec8bc310ae48f64
abdesigner <bussolari.alessio@yahoo.com>
2016-02-09 14:37:45 +0100

    Init

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validate :email_is_unique, on: :create
+  after_create :create_account
+
+  def confirmation_required?
+    false
+  end
+
+
+  private
+
+  # controllo se l'email è unica nell'ACCOUNT model
+  def email_is_unique
+    # non effettuo la validazione se è gia presente un errore relativo alla mail
+    return false unless self.errors[:email].empty?
+    # controllo se è gia presente la mail se si aggiungo l'errore di mail presente
+    unless Account.find_by_email(email).nil?
+      errors.add(:email, "la mail è già stata utilizzata")
+    end
+  end
+
+  # dopo la creazione dell'user di devise aggiungo un account
+  def create_account
+    account = Account.new(:email => email)
+    account.save!
+  end
+
+end
